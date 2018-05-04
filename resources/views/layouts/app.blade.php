0bf612cb3045cdcba32f8cf2375be50d0ed6f205
DamienToomey <35866476+DamienToomey@users.noreply.github.com>
2018-05-04 19:54:40 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,22 @@
       <h1>Matériel à disposition</h1>
     </header>
     <form action="GET">
+    
+    <div class="btn-group mr-4" data-toggle="buttons">
+
+    <label class="btn btn-primary active form-check-label">
+        <input type="checkbox" class="form-check-input" checked autocomplete="off"> Checkbox 1 (pre-checked)
+    </label>
+
+    <label class="btn btn-primary form-check-label">
+        <input type="checkbox" class="form-check-input" autocomplete="off"> Checkbox 2
+    </label>
+
+    <label class="btn btn-primary form-check-label">
+        <input type="checkbox" class="form-check-input" autocomplete="off"> Checkbox 3
+    </label>
+
+</div>
       
        <div class="form-check">
         <input type="checkbox" class="custom-control-input" name="materiel" id="1" value="Presse"/>
