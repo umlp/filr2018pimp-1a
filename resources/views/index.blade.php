62e569e1ce9e1984fadd6b4f2371cb17a5aa2a63
DamienToomey <damien.toomey@insa-rouen.fr>
2018-06-06 21:35:06 +0200

    Modifs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -92,4 +92,15 @@
      </form>
 
   </body>
+  <footer>
+      <script type="text/javascript">
+      function stopRKey(evt) {
+        var evt = (evt) ? evt : ((event) ? event : null);
+        var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+        if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+      }
+
+      document.onkeypress = stopRKey;
+    </script>
+  </footer>
 </html>
